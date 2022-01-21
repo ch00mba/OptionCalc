3fa90e39cff6567d020de851232e14af2ac87dea
ak <karyagin.finworks@gmail.com>
2022-01-21 19:39:28 +0300

    Create plot_options.py

---
+++ plot_options.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 21 19:38:37 2022
+
+@author: choomba
+"""
+
+import time
+
+
+
+ts = int(time.time())
+
+# if you encounter a "year is out of range" error the timestamp
+# may be in milliseconds, try `ts /= 1000` in that case
+print(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
