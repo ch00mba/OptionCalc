49dd1e45e2ecb7a5ef43b52e935824d9f8b35698
ak <karyagin.finworks@gmail.com>
2022-01-21 20:56:44 +0300

    Update plot_options.py

--- plot_options.py
+++ plot_options.py
@@ -15,3 +15,5 @@ ts = int(time.time())
 # if you encounter a "year is out of range" error the timestamp
 # may be in milliseconds, try `ts /= 1000` in that case
 print(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+
+
