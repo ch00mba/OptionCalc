3a9fbe405e709c32b2c9240cee422b0288270462
ak <karyagin.finworks@gmail.com>
2022-01-23 17:37:34 +0300

    added new exp columns to db

--- get_options.py
+++ get_options.py
@@ -18,77 +18,36 @@ while True:
     for i, val in enumerate(tqqq_dates_exp):
     
         timestamp = int(time.time())
-        
-        
-        
+        datetime1 = datetime.datetime.strptime(tqqq_dates_exp[i], '%B %d, %Y')
 
         opt_data = options.get_options_chain("tqqq", tqqq_dates_exp[i])
     
         df_calls = pd.DataFrame(opt_data['calls'], columns=opt_data['calls'].keys())
-        df_calls = df.replace('-', 0.00)
+        df_calls = df_calls.replace('-', 0.00)
         df_calls['snapshot_time'] = timestamp # unix time
         df_calls['calls'] = 1
-        df_calls['exp_date']
-        df_calls['exp_date_unix']
-        df_calls['strike']
+        df_calls['exp_date'] = datetime1
+        df_calls['exp_date_unix'] = int(datetime1.date().strftime("%s"))
         
-        int(datetime1.date().strftime("%s"))
+        df_calls.to_sql(val, statsEngine, if_exists='append', index=False)
+        df_calls.to_sql("all_options_test", statsEngine, if_exists='append', index=False)
+
+
+
+        df_puts = pd.DataFrame(opt_data['puts'], columns=opt_data['puts'].keys())
+        df_puts = df_puts.replace('-', 0.00)
+        df_puts['snapshot_time'] = timestamp # unix time
+        df_puts['calls'] = 0
+        df_puts['exp_date'] = datetime1
+        df_puts['exp_date_unix'] = int(datetime1.date().strftime("%s"))        
         
-        
-        df.to_sql(val, statsEngine, if_exists='append', index=False)
-        df.to_sql("all_options", statsEngine, if_exists='append', index=False)
 
-    
-
-
-
-
-        df = pd.DataFrame(opt_data['puts'], columns=opt_data['puts'].keys())
-        df = df.replace('-', 0.00)
-        df['snapshot_time'] = timestamp # unix time
-        
-        singleString = df["Contract Name"][1]
-
-        pattern = "TQQQ(.*?)P"
-
-        substring = re.search(pattern, singleString).group(1)
-       
-        print(substring)
-
-        t = iter(substring)
-        date1 = '-'.join(a+b for a,b in zip(t, t))
-        date2 = "20" + date1
-
-        #input = date2
-          
-
-        #format = '%Y-%m-%d'
-          
-
-        #datetime1 = datetime.datetime.strptime(input, format)
-        
-        datetime1 = datetime.datetime.strptime(tqqq_dates_exp[1], '%B %d, %Y')
-        
-        
-        
-        
-        
-        tqqq_dates_exp[1]
-          
-
-        print(datetime1.date())
-        
-        df.to_sql(val, statsEngine, if_exists='append', index=False)
-        df.to_sql("all_options", statsEngine, if_exists='append', index=False)
+        df_puts.to_sql(val, statsEngine, if_exists='append', index=False)
+        df_puts.to_sql("all_options", statsEngine, if_exists='append', index=False)
 
     postgreSQLConnection.close()
     time.sleep(900)
 
 
-#TODO
-
-# create one big table for everyting 
-
-# create table for each expiation puts and calls separately
 
 # convurent version https://stackoverflow.com/questions/20170251/how-to-run-a-script-forever
